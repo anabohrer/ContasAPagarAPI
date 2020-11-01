04891be54550f1487bddf42947acfa2c5fac4b4a
Ana Luiza Bohrer <ana.bohrer@hammerconsult.com.br>
2020-10-31 21:32:52 -0300

    Inclusão de validação de campos de data e valor

--- ContasAPagarAPI/Dtos/ContaPagaCreateDto.cs
+++ ContasAPagarAPI/Dtos/ContaPagaCreateDto.cs
@@ -8,9 +8,16 @@ namespace ContasAPagarAPI.Dtos
         [Required(ErrorMessage = "Campo nome é obrigatório")]
         [MaxLength(120)]
         public string Nome { get; set; }
+        [Range(0.1, double.MaxValue, ErrorMessage = "O Valor Original precisa ser maior que zero")]
         public double ValorOriginal { get; set; }
+
+        [Range(typeof(DateTime), "1/1/2015", "31/12/2022",
+                 ErrorMessage = "Data de Vencimento precisa ser entre {0} {1:d} e {2:d}")]
         public DateTime DataVencimento { get; set; }
+        [Range(typeof(DateTime), "1/1/2015", "31/12/2022",
+                ErrorMessage = "Data de Pagamento precisa ser entre {0} {1:d} e {2:d}")]
         public DateTime DataPagamento { get; set; }
+
         public double ValorCorrigido
         {
             get
